9b4bf3fd57ca7132b514ba6402205df7d499d5d6
Batu <miliev@ethz.ch>
2026-04-03 15:06:43 +0200

    add installation script for GB10

---
+++ prep_venv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Use only for GB10 GPU
+python3 -m venv venv_gb10
+source venv_gb10/bin/activate
+
+pip install torch==2.7.1 torchvision==0.22.1 --index-url https://download.pytorch.org/whl/cu128
+pip install -r requirements.txt
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.1+cu128.html
+pip install gsplat
+pip install chumpy --no-build-isolation
+pip install pyyaml
