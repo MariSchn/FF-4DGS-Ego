34d52c1c9c0a532b8301fe2517f88632681b0f7a
Batu <miliev@ethz.ch>
2026-04-03 15:04:54 +0200

    add initial run logs

--- batch.sh
+++ batch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Select which GPU to use
 #SBATCH --gpus=5060ti:1
 # SBATCH --gpus=gb10:1
+
 #SBATCH --time=24:00:00
 #SBATCH --account=3dv
 #SBATCH --job-name=ff4dgs
-#SBATCH --output=logs/%j.out
+#SBATCH --output=logs/%j.out    # Make sure the logs directory exists!
 #SBATCH --error=logs/%j.err
 
 source venv/bin/activate
