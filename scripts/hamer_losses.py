27009590a20b5a3703fc9a508c829463ba4f3acd
Batu <miliev@ethz.ch>
2026-04-16 10:48:56 +0200

    change loss reduction type to mean

--- scripts/hamer_losses.py
+++ scripts/hamer_losses.py
@@ -8,7 +8,37 @@ import torch
 import torch.nn as nn
 
 
+class Keypoint2DLoss(nn.Module):
 
+    def __init__(self, loss_type: str = 'l1'):
+        """
+        2D keypoint loss module.
+        Args:
+            loss_type (str): Choose between l1 and l2 losses.
+        """
+        super(Keypoint2DLoss, self).__init__()
+        if loss_type == 'l1':
+            self.loss_fn = nn.L1Loss()
+        elif loss_type == 'l2':
+            self.loss_fn = nn.MSELoss()
+        else:
+            raise NotImplementedError('Unsupported loss function')
+
+    def forward(self, pred_keypoints_2d: torch.Tensor, gt_keypoints_2d: torch.Tensor) -> torch.Tensor:
+        """
+        Compute 2D reprojection loss on the keypoints.
+        Args:
+            pred_keypoints_2d (torch.Tensor): Tensor of shape [B, S, N, 2] containing projected 2D keypoints (B: batch_size, S: num_samples, N: num_keypoints)
+            gt_keypoints_2d (torch.Tensor): Tensor of shape [B, S, N, 3] containing the ground truth 2D keypoints and confidence.
+        Returns:
+            torch.Tensor: 2D keypoint loss.
+        """
+        conf = gt_keypoints_2d[:, :, -1].unsqueeze(-1).clone()
+        batch_size = conf.shape[0]
+        loss = (conf * self.loss_fn(pred_keypoints_2d, gt_keypoints_2d[:, :, :-1])).sum(dim=(1,2))
+        return loss.sum()
+
+        
 class Keypoint3DLoss(nn.Module):
     def __init__(self, loss_type: str = 'l1'):
         """
@@ -19,9 +49,9 @@ class Keypoint3DLoss(nn.Module):
         """
         super(Keypoint3DLoss, self).__init__()
         if loss_type == 'l1':
-            self.loss_fn = nn.L1Loss(reduction='none')
+            self.loss_fn = nn.L1Loss()
         elif loss_type == 'l2':
-            self.loss_fn = nn.MSELoss(reduction='none')
+            self.loss_fn = nn.MSELoss()
         else:
             raise NotImplementedError(f'Unsupported loss type: {loss_type!r}. Choose from "l1" or "l2".')
 
@@ -75,7 +105,7 @@ class ParameterLoss(nn.Module):
         annotation is unavailable.
         """
         super(ParameterLoss, self).__init__()
-        self.loss_fn = nn.MSELoss(reduction='none')
+        self.loss_fn = nn.MSELoss()
 
     def forward(
         self,
